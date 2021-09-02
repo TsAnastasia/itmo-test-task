0af952fec66a43a9edfbb2cd5258e84d6a0fffae
Anastasia Tsibernaya <yfcnz68@yandex.ru>
2021-09-03 01:05:55 +0300

    feat: create auth reducer

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -1,5 +1,34 @@
-const initialStore = { isAuth: true };
+import * as types from "../types";
+
+const token = localStorage.getItem("token");
+
+const initialStore = { isAuth: !!token, user: null, token };
 
 export default function auth(state = initialStore, action) {
-  return state;
+  switch (action.type) {
+    case types.SIGNIN_SUCCESS:
+      return {
+        ...state,
+        isAuth: true,
+        user: action.payload.user,
+        token: action.payload.token,
+      };
+    case types.RECIEVE_AUTH_SUCCESS:
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user,
+      };
+    case types.SIGNIN_FAILURE:
+    case types.RECIEVE_AUTH_FAILURE:
+    case types.LOGOUT_SUCCESS:
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+        token: "",
+      };
+    default:
+      return state;
+  }
 }
---
+++ src/redux/types.js
@@ -0,0 +1,11 @@
+export const SIGNIN_REQUEST = "auth/LOGIN_REQUEST";
+export const SIGNIN_SUCCESS = "auth/LOGIN_SUCCESS";
+export const SIGNIN_FAILURE = "auth/LOGIN_FAILURE";
+
+export const LOGOUT_REQUEST = "auth/LOGOUT_REQUEST";
+export const LOGOUT_SUCCESS = "auth/LOGOUT_SUCCESS";
+export const LOGOUT_FAILURE = "auth/LOGOUT_FAILURE";
+
+export const RECIEVE_AUTH_REQUEST = "auth/RECIEVE_AUTH_REQUEST";
+export const RECIEVE_AUTH_SUCCESS = "auth/RECIEVE_AUTH_SUCCESS";
+export const RECIEVE_AUTH_FAILURE = "auth/RECIEVE_AUTH_FAILURE";
