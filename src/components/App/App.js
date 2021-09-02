a78862921a3e37f7c2dee109179b6085f00032b1
Anastasia Tsibernaya <yfcnz68@yandex.ru>
2021-09-02 22:59:04 +0300

    feat: default configure

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="app">
+      <header className="app__header"></header>
+      <main className="app__main">
+        <h1 className="app__title">React app</h1>
+      </main>
+      <footer className="app__footer"></footer>
+    </div>
+  );
+};
+
+export default App;
