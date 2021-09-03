8cb67746ed78cea370012865231e82c7e06be3b6
Anastasia Tsibernaya <yfcnz68@yandex.ru>
2021-09-03 09:59:27 +0300

    feat: add main page nav

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import MainPageNav from "../MainPageNav/MainPageNav";
 
 const MainPage = () => {
+  const isAuth = useSelector((state) => state.auth.isAuth);
+
   return (
     <main className="main-page">
-      <nav className="main-page__nav"></nav>
+      <MainPageNav />
       <section className="main-page__content">
         <h1 className="app__title">Welcome to the ...</h1>
       </section>
